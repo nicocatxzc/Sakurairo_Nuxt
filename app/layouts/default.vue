b3bb7d6a983cf5ed2f50ebc28bcef9c5df2ee832
nicocatxzc <nicocatxzc@163.com>
2025-12-09 19:07:06 +0800

    修改为dv单位

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,18 +2,19 @@
 
 <template>
     <div class="background">
-        <div class="background-filter"></div>
+        <div class="background-filter"/>
         <ProgressBar class="progress-bar" />
         <NavBar class="nav-bar" />
         <slot class="layout-slot" />
+        <SiteFooter class="site-footer"/>
     </div>
 </template>
 
 <style scoped>
 .background-filter {
     position: fixed;
-    height: 100vh;
-    width: 100vw;
+    height: 100dvh;
+    width: 100dvw;
     background-color: rgba(255, 255, 255, 0.7);
     backdrop-filter: saturate(120%) blur(8px);
 }
