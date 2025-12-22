7d30117c33928d0a0dfbb594e36ebb4c5580c7af
nicocatxzc <nicocatxzc@163.com>
2025-12-23 01:19:16 +0800

    修改ESlint规则，防止和prettier冲突

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,21 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  // Your custom configs here
-)
+    // Your custom configs here
+    {
+        rules: {
+            "prefer-const": "off",
+            "vue/html-self-closing": [
+                "error",
+                {
+                    html: {
+                        void: "always",
+                        normal: "any",
+                        component: "any",
+                    },
+                },
+            ],
+        },
+    }
+);
