79b458d125b77ad17a8d63f98b87ffaa55a77f97
nicocatxzc <nicocatxzc@163.com>
2025-12-28 18:28:36 +0800

    主页布局接入设置

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,16 +6,22 @@ const api = {
         promise: true,
     },
 };
-const themeConfig=useThemeConfig()
+const themeConfig = useThemeConfig();
 const { data, promise } = await useCachedFetch(api.key, api.url, api.option);
 await promise;
 const posts = computed(() => (data.value?.posts ? data.value.posts : {}));
 
 const title = themeConfig.value?.siteName || "未命名";
-const desc = themeConfig.value?.siteSeoDesc || themeConfig.value?.siteDesc || "这家伙很懒,什么都没有写";
-const icon = themeConfig.value?.siteLogo || themeConfig.value?.navLogo || `${useSiteOrigin()}/favicon.ico`
+const desc =
+    themeConfig.value?.siteSeoDesc ||
+    themeConfig.value?.siteDesc ||
+    "这家伙很懒,什么都没有写";
+const icon =
+    themeConfig.value?.siteLogo ||
+    themeConfig.value?.navLogo ||
+    `${useSiteOrigin()}/favicon.ico`;
 useHead({
-    title:title,
+    title: title,
     meta: [
         { name: "description", content: desc },
         { name: "keywords", content: themeConfig.value?.siteSeoKeyword },
@@ -23,24 +29,47 @@ useHead({
         { property: "og:title", content: title },
         { property: "og:image", content: icon },
         { property: "og:url", content: `${useSiteOrigin()}/` },
-        { property: "og:type", content: 'website' },
+        { property: "og:type", content: "website" },
         { property: "og:site_name", content: title },
     ],
-    link:[{
-        rel:"canonical",href:useSiteOrigin()+'/'
-    }]
+    link: [
+        {
+            rel: "canonical",
+            href: useSiteOrigin() + "/",
+        },
+    ],
 });
 </script>
 
 <template>
-    <div>
-        <ContentContainer>
-            <h2 class="block-title">展栏</h2>
-            <HomepageShow />
-            <h2 class="block-title">破事水</h2>
-            <PostList :post-list="posts" :api="api" />
-        </ContentContainer>
-    </div>
+    <ContentContainer
+        :style="{
+            '--block-title-font': themeConfig?.blockTitleFont || '',
+            '--block-title-position': themeConfig?.blockTitlePosition || 'center'
+        }"
+    >
+        <template
+            v-for="(block, index) in themeConfig?.homepageBlock || [
+                'show',
+                'postlist',
+                'custom',
+            ]"
+            :key="index"
+        >
+            <template v-if="block == 'show'">
+                <h2 class="block-title flex-center">
+                    {{ themeConfig?.showBlockTitle || "展栏" }}
+                </h2>
+                <HomepageShow />
+            </template>
+            <template v-if="block == 'postlist'">
+                <h2 class="block-title flex-center">
+                    {{ themeConfig?.postListBlockTitle || "文章" }}
+                </h2>
+                <PostList :post-list="posts" :api="api" />
+            </template>
+        </template>
+    </ContentContainer>
 </template>
 
 <style scoped>
@@ -48,11 +77,8 @@ useHead({
     font-size: 1.2rem;
     width: 100%;
     margin-top: 2rem;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
     padding-bottom: 0.6rem;
+
+    justify-content: var(--block-title-position);
 }
 </style>
