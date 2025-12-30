ad32cc2a244ff9b7c8f805a0e4db81cd58876593
nicocatxzc <nicocatxzc@163.com>
2025-12-30 21:56:21 +0800

    修复设置页编辑器的语言高亮问题

--- app/formkit/inputs/CodeEditor.vue
+++ app/formkit/inputs/CodeEditor.vue
@@ -21,20 +21,21 @@ watch(
 </script>
 
 <template>
-    <div style="border: 1px solid #ccc;">
-        {{ props.context.language }}
-        <MonacoEditor
-            :model-value="props.context._value ?? ''"
-            :language="props.context.language || 'jsx'"
-            :options="{
-                minimap: { enabled: false },
-                lineNumbers: 'on',
-                scrollBeyondLastLine: false,
-                automaticLayout: true,
-            }"
-            class-name="editor"
-            style="height: 12.5rem"
-            @update:model-value="handleUpdate"
-        />
-    </div>
+    <ClientOnly>
+        <div style="border: 1px solid #ccc">
+            <MonacoEditor
+                :model-value="props.context._value ?? ''"
+                :lang="props.context.language || 'jsx'"
+                :options="{
+                    minimap: { enabled: false },
+                    lineNumbers: 'on',
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                }"
+                class-name="editor"
+                style="height: 12.5rem"
+                @update:model-value="handleUpdate"
+            />
+        </div>
+    </ClientOnly>
 </template>
