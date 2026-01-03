e59955352fe704ef2f2f8c5b40f8a7a3447e80fc
nicocatxzc <nicocatxzc@163.com>
2026-01-03 22:43:57 +0800

    为后台管理页添加登录提示

---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+onMounted(() => {
+    const auth = useAuth();
+    if (auth.expire == 0) {
+        ElMessage.info("请先登录！");
+        auth.loginFormState = true;
+    }
+    let stopwatch = watch(
+        () => auth.loginFormState,
+        () => {
+            if (auth.loginFormState == false && auth.expire == 0) {
+                navigateTo("/");
+            }
+        }
+    );
+    onUnmounted(()=>{
+        stopwatch()
+    })
+});
+</script>
+
+<template>
+    <div id="dashboard">
+        <slot />
+        <ClientOnly>
+            <SiteModels />
+        </ClientOnly>
+    </div>
+</template>
+
+<style scoped></style>
